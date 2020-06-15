8f2d82b0a101c983c943405dbdc18d8618b4910c
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2020-06-14 21:39:13 -0300

    Particula - Tornando a movimentação Bi dimencional

--- js/models/Particula.js
+++ js/models/Particula.js
@@ -2,15 +2,22 @@ class Particula {
 
     constructor(context) {
         this.drawContext = context;
-        this.x = Math.random() * this.drawContext.canvas.clientWidth;
-        this.y = Math.random() * this.drawContext.canvas.clientHeight;
         this.radius = 10;
-        this.speed = 10;
+        this.vPosition = {
+            x: Math.random() * this.drawContext.canvas.clientWidth,
+            y: Math.random() * this.drawContext.canvas.clientHeight
+        };        
+        this.speed = 3;
+        this.directionAngle = Math.floor(Math.random() * 360);
+        this.vSpeed = {
+            x: (Math.cos(this.directionAngle) * this.speed),
+            y: (Math.sin(this.directionAngle) * this.speed)
+        }
     }
 
     draw() {
         this.drawContext.beginPath();
-        this.drawContext.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+        this.drawContext.arc( this.vPosition.x, this.vPosition.y, this.radius, 0, Math.PI*2);
         this.drawContext.closePath();
         this.drawContext.fillStyle = '#FFF';
         this.drawContext.fill();
@@ -19,6 +26,7 @@ class Particula {
     }
 
     update() {
-        this.x += this.speed; 
+        this.vPosition.x += this.vSpeed.x; 
+        this.vPosition.y += this.vSpeed.y; 
     }
 }
